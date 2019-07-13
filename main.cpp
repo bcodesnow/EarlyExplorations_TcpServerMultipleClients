b49bb35d1b123eeb9b10117d73da1dbcfbee9c9d
bcodesnow <50692300+bcodesnow@users.noreply.github.com>
2019-07-13 04:04:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//#include <QCoreApplication>
+#include "tcpreceiver.h"
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    TCPReceiver trec;
+
+    while(1)
+    {
+
+    }
+    //return a.exec();
+}
---
+++ tcpreceiver.h
@@ -0,0 +1,231 @@
+#ifndef TCPRECEIVER_H
+#define TCPRECEIVER_H
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <assert.h>
+#include <string.h>
+#include <ctime>
+#include <pthread.h>
+#include <iostream>
+#include "../RaspiSerial/raspiserial.h"
+
+#define TRUE   1
+#define FALSE  0
+#define SERVERPORT 8887
+
+class TCPReceiver
+{
+
+private:
+
+    RaspiSerial*        m_ref_to_raspiSerial;
+    uint8_t             m_isSerialActive;
+
+    int                 serverSock;
+    int                 key;
+    pthread_t           thread_rec;
+    bool*               start_flag;
+    pthread_mutex_t     job_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    typedef void * (*THREADFUNCPTR)(void *);
+
+    void* receiverThread(void* arg)
+    {
+        int opt = TRUE;
+        int master_socket , addrlen , new_socket , client_socket[30] ,
+              max_clients = 30 , activity, i , valread , sd;
+        int max_sd;
+        struct sockaddr_in address;
+
+        char buffer[1025];  //data buffer of 1K
+
+        //set of socket descriptors
+        fd_set readfds;
+
+        //a message
+        char *message = "Neato Controller v0b - TCP_IP to Serial0 \r\n";
+        printf("Neato Controller v0b - TCP_IP to Serial0 Thread Strating Up.. \r\n");
+
+        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+        //initialise all client_socket[] to 0 so not checked
+        for (i = 0; i < max_clients; i++)
+        {
+            client_socket[i] = 0;
+        }
+
+        //create a master socket
+        if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        //set master socket to allow multiple connections ,
+        //this is just a good habit, it will work without this
+        if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
+              sizeof(opt)) < 0 )
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        //type of socket created
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( SERVERPORT );
+
+        //bind the socket to localhost port 8888
+        if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("Listener on port %d \n", SERVERPORT);
+
+        //try to specify maximum of 3 pending connections for the master socket
+        if (listen(master_socket, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        //accept the incoming connection
+        addrlen = sizeof(address);
+        puts("Waiting for connections ...");
+
+        while(1)
+        {
+            //clear the socket set
+            FD_ZERO(&readfds);
+
+            //add master socket to set
+            FD_SET(master_socket, &readfds);
+            max_sd = master_socket;
+
+            //add child sockets to set
+            for ( i = 0 ; i < max_clients ; i++)
+            {
+                //socket descriptor
+                sd = client_socket[i];
+
+                //if valid socket descriptor then add to read list
+                if(sd > 0)
+                    FD_SET( sd , &readfds);
+
+                //highest file descriptor number, need it for the select function
+                if(sd > max_sd)
+                    max_sd = sd;
+            }
+
+            //wait for an activity on one of the sockets , timeout is NULL ,
+            //so wait indefinitely
+            activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+
+            if ((activity < 0) && (errno!=EINTR))
+            {
+                printf("select error");
+            }
+
+            //If something happened on the master socket ,
+            //then its an incoming connection
+            if (FD_ISSET(master_socket, &readfds))
+            {
+                if ((new_socket = accept(master_socket,
+                        (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+                {
+                    printf("ERR1");
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+
+                //inform user of socket number - used in send and receive commands
+                printf("New connection , socket fd is %d , ip is : %s , port : %d\n",
+                       new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+                //send new connection greeting message
+                if( send(new_socket, message, strlen(message), 0) != strlen(message) )
+                {
+                    perror("send");
+                    printf("ERR2");
+                }
+
+                puts("Welcome message sent successfully");
+
+                //add new socket to array of sockets
+                for (i = 0; i < max_clients; i++)
+                {
+                    //if position is empty
+                    if( client_socket[i] == 0 )
+                    {
+                        client_socket[i] = new_socket;
+                        printf("Adding to list of sockets as %d\n" , i);
+
+                        break;
+                    }
+                }
+            }
+
+            //else its some IO operation on some other socket
+            for (i = 0; i < max_clients; i++)
+            {
+                sd = client_socket[i];
+
+                if (FD_ISSET( sd , &readfds))
+                {
+                    //Check if it was for closing , and also read the
+                    //incoming message
+                    //printf("Check for disconnects");
+                    if ((valread = read( sd , buffer, 1024)) == 0)
+                    {
+                        //Somebody disconnected , get his details and print
+                        getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
+                        printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+                        //Close the socket and mark as 0 in list for reuse
+                        close( sd );
+                        client_socket[i] = 0;
+                    }
+
+                    //Echo back the message that came in
+                    else
+                    {
+                        //set the string terminating NULL byte on the end
+                        //of the data read
+                        buffer[valread] = '\0';
+                        //std::cout<<"Message arived, length:"<<strlen(buffer)<<std::endl;
+                        //send(sd , buffer , strlen(buffer) , 0 );
+                        if ( m_isSerialActive )
+                            m_ref_to_raspiSerial->add_job(&buffer[0], strlen(buffer));
+
+                    }
+                }
+            }
+            //printf("trying to cancel");
+            usleep(100);
+            pthread_testcancel();
+        }
+
+        return 0;
+    }
+
+
+public:
+    void set_ref_to_raspiSerial (RaspiSerial* ref)
+    {
+        m_ref_to_raspiSerial = ref;
+        m_isSerialActive = true;
+    }
+    TCPReceiver()
+    {
+        if (pthread_create(&this->thread_rec, NULL, (THREADFUNCPTR) &TCPReceiver::receiverThread, this))
+            std::cout<<"now we should quit"<<std::endl; //quit("\n--> pthread_create failed.", 1);
+    }
+};
+
+#endif // TCPRECEIVER_H
